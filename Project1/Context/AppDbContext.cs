fcd40ee8de1fda9d164aae169a95451d69f25dba
Jlgacha <jlgacha75@gmail.com>
2023-05-09 22:54:29 +0200

    Initial commit

---
+++ Project1/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project1.Models;
+
+namespace Project1.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+        
+        }
+        public DbSet<Usuarios> usuarios { get; set; }
+    }
+}
---
+++ Project1/Models/Usuarios.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project1.Models
+{
+    [Table("usuarios")]
+    public class Usuarios
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string mail { get; set; }
+        public string password { get; set; }
+    }
+}
