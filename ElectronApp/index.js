9d1a4ee6ae0ac14a2b9fe688baa8bf904a05a929
Jlgacha <jlgacha75@gmail.com>
2023-05-10 11:47:04 +0200

    Update index.js

--- ElectronApp/index.js
+++ ElectronApp/index.js
@@ -3,9 +3,10 @@ const { app, BrowserWindow } = require('electron')
 function createWindow() {
   // Crea la ventana del navegador.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    autoHideMenuBar: true,
+    width: 1920, // Ancho de pantalla completa
+    height: 1080, // Alto de pantalla completa
+    autoHideMenuBar: true, // Oculta la barra de herramientas
+    show: false,
     webPreferences: {
       nodeIntegration: true
     }
@@ -13,6 +14,8 @@ function createWindow() {
 
   // Carga la página de tu aplicación ASP.NET y React.
   mainWindow.loadURL('https://localhost:44443')
+  mainWindow.maximize()
+  mainWindow.show()
 }
 
 // Cuando Electron haya terminado de inicializarse, crea la ventana de tu aplicación.
