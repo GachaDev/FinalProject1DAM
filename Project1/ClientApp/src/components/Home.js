d032834b01f52c038d5c528c1855bb17fcd8b1e0
Jlgacha <jlgacha75@gmail.com>
2023-05-18 12:45:48 +0200

    front calendar

--- Project1/ClientApp/src/components/Home.js
+++ Project1/ClientApp/src/components/Home.js
@@ -1,45 +1,20 @@
 import { useState } from 'react';
-import dayjs from 'dayjs';
-import { createStyles, Image, Text, Paper, Group, rem } from '@mantine/core';
-import { Title } from '@mantine/core';
-
-import {
-  IconSwimming,
-  IconBike,
-  IconRun,
-  IconChevronDown,
-  IconChevronUp,
-} from '@tabler/icons-react';
-import { Grid } from '@mantine/core';
-
-const useStyles = createStyles((theme) => ({
-  root: {
-    borderRadius: theme.radius.md,
-    width: 320,
-    display: 'flex',
-    flexDirection: 'column',
-    marginLeft: 10,
-    borderRadius: "8px",
-    boxShadow: "0px 0px 4px rgba(0, 0, 0, 0.25)"
-  },
-
-  stat: {
-    display: 'flex',
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: theme.white
-  },
-  
-}));
-
+import { Title, Image, ActionIcon } from '@mantine/core';
+import { ChevronLeft, ChevronRight } from 'tabler-icons-react';
 export default function Home() {
-  const { classes } = useStyles();
   return (
     <div style={{ display: 'flex' }}>
       <div style={{ flex: '3' }}>
         <div className='containerCalendar'>
           <Title order={4} className='titleCalendar'>Próximos partidos</Title>
-          <p style={{textAlign: 'center'}}>Jornada 3</p>
+          <div className='jornadaInfo'>
+            <ActionIcon variant="transparent"><ChevronLeft size="1rem" /></ActionIcon>
+            <div>
+              <span className='titleJornada'>Jornada 3</span>
+              <span className='dateJornada' style={{textAlign: 'center', fontSize: 12}}>21 de Mayo de 2023</span>
+            </div>
+            <ActionIcon variant="transparent"><ChevronRight size="1rem" /></ActionIcon>
+          </div>
           <div className='match'>
             <Image maw={40} mx="auto" radius="md" src="https://kingsleague.pro/wp-content/uploads/2022/11/xbuyer-team.svg" alt="Random image" />
             <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: "column" }}>
@@ -50,35 +25,35 @@ export default function Home() {
           <div className='match'>
               <Image maw={40} mx="auto" radius="md" src="https://kingsleague.pro/wp-content/uploads/2022/11/el-bbarrio.svg" alt="Random image" />
             <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-              <span>VS</span>
+              <span>17:00</span>
             </div>
               <Image maw={40} mx="auto" radius="md" src="https://kingsleague.pro/wp-content/uploads/2022/11/jijantes-fc.svg" alt="Random image" />
           </div>
           <div className='match'>
               <Image maw={40} mx="auto" radius="md" src="https://kingsleague.pro/wp-content/uploads/2022/11/ultimate-mostoles.svg" alt="Random image" />
             <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-              <span>VS</span>
+              <span>18:00</span>
             </div>
               <Image maw={40} mx="auto" radius="md" src="https://kingsleague.pro/wp-content/uploads/2022/11/aniquiladores.svg" alt="Random image" />
           </div>
           <div className='match'>
               <Image maw={40} mx="auto" radius="md" src="https://kingsleague.pro/wp-content/uploads/2022/11/kunisports.svg" alt="Random image" />
             <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-              <span>VS</span>
+              <span>19:00</span>
             </div>
               <Image maw={40} mx="auto" radius="md" src="https://kingsleague.pro/wp-content/uploads/2022/11/rayo-barcelona.svg" alt="Random image" />
           </div>
           <div className='match'>
               <Image maw={40} mx="auto" radius="md" src="https://kingsleague.pro/wp-content/uploads/2022/11/pio.svg" alt="Random image" />
             <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-              <span>VS</span>
+              <span>20:00</span>
             </div>
               <Image maw={40} mx="auto" radius="md" src="https://kingsleague.pro/wp-content/uploads/2022/11/1k.svg" alt="Random image" />
           </div>
           <div className='match'>
               <Image maw={40} mx="auto" radius="md" src="https://kingsleague.pro/wp-content/uploads/2022/11/porcinos-fc.svg" alt="Random image" />
             <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-              <span>VS</span>
+              <span>21:00</span>
             </div>
               <Image maw={40} mx="auto" radius="md" src="https://kingsleague.pro/wp-content/uploads/2022/12/los-troncos.png" alt="Random image" />
           </div>
