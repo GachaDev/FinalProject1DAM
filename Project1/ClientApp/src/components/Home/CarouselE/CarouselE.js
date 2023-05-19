94abefeb46b8aebed9120591e5d856b03a26cc59
Jlgacha <jlgacha75@gmail.com>
2023-05-19 23:52:42 +0200

    Fixeado el carousel de nuevo ya si que si la definitiva

--- Project1/ClientApp/src/components/Home/CarouselE/CarouselE.js
+++ Project1/ClientApp/src/components/Home/CarouselE/CarouselE.js
@@ -30,7 +30,7 @@ const useStyles = createStyles((theme) => ({
   },
 }));
 
-function Card({ image, title }) {
+function Card({ image, title, date }) {
   const { classes } = useStyles();
 
   return (
@@ -45,7 +45,7 @@ function Card({ image, title }) {
         <Title order={3} className={classes.title}>
           {title}
         </Title>
-        <Text className={classes.date}>May 18, 2023</Text>
+        <Text className={classes.date}>{date}</Text>
       </div>
     </Paper>
   );
@@ -53,39 +53,92 @@ function Card({ image, title }) {
 
 const data = [
   {
+    id: 1,
     image: 'https://kingsleague.pro/wp-content/uploads/2023/05/KL_J2.jpg',
     title: '1K se coloca líder en la jornada más goleadora',
+    date: '14 MAYO 2023'
   },
   {
+    id: 2,
     image: 'https://kingsleague.pro/wp-content/uploads/2023/05/16831283669188.jpg',
     title: 'Djibril Cissé, destacado en las compras para la 2ª jornada',
+    date: '13 MAYO 2023'
   },
   {
-    image: 'https://kingsleague.pro/wp-content/uploads/2023/05/16831283669188.jpg',
-    title: 'Djibril Cissé, destacado en las compras para la 2ª jornada',
+    id: 3,
+    image: 'https://kingsleague.pro/wp-content/uploads/2023/05/Fvit7yDXwAE3E5L.jpeg',
+    title: '¡La Kings League InfoJobs volvió por todo lo alto!',
+    date: '08 MAYO 2023'
+  },
+  {
+    id: 4,
+    image: 'https://kingsleague.pro/wp-content/uploads/2023/04/022_MercatoKings1_RachelAlvhz-scaled.jpg',
+    title: 'Se acabó el ‘mercato’: ya tenemos plantillas definitivas',
+    date: '26 ABRIL 2023'
+  },
+  {
+    id: 5,
+    image: 'https://kingsleague.pro/wp-content/uploads/2023/04/MQJM00394.jpg',
+    title: 'Novedades del reglamento para el 2º split',
+    date: '24 ABRIL 2023'
+  },
+  {
+    id: 6,
+    image: 'https://kingsleague.pro/wp-content/uploads/2023/04/J12-y-cronicas-2023-04-18T090628.652-1.jpg',
+    title: 'Así será el innovador playoff del 2º split',
+    date: '19 ABRIL 2023'
+  },
+  {
+    id: 7,
+    image: 'https://kingsleague.pro/wp-content/uploads/2023/04/MercatoKings_dia1.jpg',
+    title: 'Mercato: ¡Acuerdo múltiple entre Jijantes, Porcinos, Aniquiladores y El Barrio!',
+    date: '13 ABRIL 2023'
+  },
+  {
+    id: 8,
+    image: 'https://kingsleague.pro/wp-content/uploads/2023/04/queens-1-e1681152528960.jpeg',
+    title: 'Estas son las jugadoras del draft de la Queens League Oysho',
+    date: '10 ABRIL 2023'
+  },
+  {
+    id: 9,
+    image: 'https://kingsleague.pro/wp-content/uploads/2023/04/PSMQ05486-e1681119398469.jpg',
+    title: 'Edgar Alvaro ficha por el Rayo y Marc Pelaz por Los Troncos',
+    date: '10 ABRIL 2023'
+  },
+  {
+    id: 10,
+    image: 'https://kingsleague.pro/wp-content/uploads/2023/04/Ubon_F4.jpg',
+    title: 'Cristian Ubón ficha por Ultimate Móstoles',
+    date: '05 ABRIL 2023'
   },
 ];
+
 
 export default function CarouselE() {
   const theme = useMantineTheme();
   const mobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
-  const [width, setWidth] = useState(mobile ? 600 : 800);
   const [forceUpdate, setForceUpdate] = useState(false);
+  const [firstTime, setFirstTime] = useState(false);
 
   useEffect(() => {
     function handleResize() {
-      setWidth(mobile ? 600 : 800);
-      setForceUpdate(prevState => !prevState);
+      setTimeout(() => setForceUpdate((prevState) => !prevState), 1);
+    }
+
+    if (!firstTime) {
+      setTimeout(() => setForceUpdate((prevState) => !prevState), 600);
+      setFirstTime(true);
     }
 
     window.addEventListener('resize', handleResize);
     return () => {
       window.removeEventListener('resize', handleResize);
     };
-  }, [mobile]);
+  }, [mobile, firstTime]);
 
   const slides = data.map((item) => (
-    <Carousel.Slide key={item.title}>
+    <Carousel.Slide key={item.id}>
       <Card {...item} />
     </Carousel.Slide>
   ));
