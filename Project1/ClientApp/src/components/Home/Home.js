5f314a15bf5e435b615b59c3d4b36181040cf642
JesusChaves <82162116+GachaDev@users.noreply.github.com>
2023-05-22 17:13:48 +0200

    cards fixed

--- Project1/ClientApp/src/components/Home/Home.js
+++ Project1/ClientApp/src/components/Home/Home.js
@@ -2,29 +2,17 @@ import Matches from './Matches/Matches';
 import CarouselE from './CarouselE/CarouselE'
 import { useMediaQuery } from '@mantine/hooks';
 import { useMantineTheme } from '@mantine/core';
-import { createStyles, Paper, Text, Title, Image, rem } from '@mantine/core';
+import { createStyles, Paper, Text, Title, Image, rem, Space } from '@mantine/core';
 
 const useStyles = createStyles((theme) => ({
-  card: {
-    height: rem(440),
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    alignItems: 'flex-start',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    padding: 0,
-    height: "auto"
-  },
-
   title: {
+    position: 'absolute',
     fontFamily: `Greycliff CF ${theme.fontFamily}`,
     fontWeight: 900,
     color: theme.white,
-    lineHeight: 1.2,
-    fontSize: "17px",
-    marginTop: theme.spacing.xs,
-    marginLeft: 5
+    lineHeight: 1.5,
+    fontSize: "12px",
+    marginLeft: 2
   },
 
   category: {
@@ -33,8 +21,7 @@ const useStyles = createStyles((theme) => ({
     color: theme.white,
     lineHeight: 1.2,
     fontSize: "25px",
-    marginTop: theme.spacing.xs,
-    marginLeft: 5
+    marginLeft: 2
   },
 }));
 
@@ -50,69 +37,87 @@ export default function Home() {
       <div style={{ flex: '2', maxWidth: '100%', minWidth: 0 }}>
         <CarouselE />
         <div style={{display: 'flex', flexDirection: 'row', gap: 20, marginTop: 20}}>
-          <Paper
-            shadow="md"
-            p="xl"
-            radius="md"
-            sx={{ backgroundImage: `url(https://kingsleague.pro/wp-content/uploads/2022/12/1k-bg.jpg)` }}
-            className={classes.card}
-          >
-            <div style={{display: "flex", flexDirection: "row"}}>
-              <div>
-                <Text className={classes.category} size="xs">
-                  Pepe
-                </Text>
-                <Title order={3} className={classes.title}>
-                  Pepito
-                </Title>
+          <div style={{ flex: '1' }}>
+            <Paper
+              shadow="md"
+              p="xl"
+              radius="md"
+              sx={{ backgroundImage: `url(	https://kingsleague.pro/wp-content/uploads/2022/12/troncos-bg.jpg)` }}
+              className={'cardMain'}
+            >
+              <div style={{display: "flex", flexDirection: "row"}}>
+                <div>
+                  <Text className={classes.category} size="xs">
+                    MVP
+                  </Text>
+                  <Text order={5} className={classes.title}>
+                    Jornada 2
+                  </Text>
+                  <p></p>
+                  <Title order={4} className={classes.title}>
+                    Joan Verdú
+                  </Title>
+                </div>
+                <div>
+                  <Image height={rem(320)} className='imageCard' src="https://kingsleague.pro/wp-content/uploads/2023/05/TFC_10.png" alt="Random image" />
+                </div>
               </div>
-              <div>
-                <Image width={"100%"} src="https://kingsleague.pro/wp-content/uploads/2023/05/1K_01.png" alt="Random image" />
+            </Paper>
+          </div>
+          <div style={{ flex: '1' }}>
+            <Paper
+              shadow="md"
+              p="xl"
+              radius="md"
+              sx={{ backgroundImage: `url(https://kingsleague.pro/wp-content/uploads/2022/12/1k-bg.jpg)` }}
+              className={'cardMain'}
+            >
+              <div style={{display: "flex", flexDirection: "row"}}>
+                <div>
+                  <Text className={classes.category} size="xs">
+                    5
+                  </Text>
+                  <Text order={5} className={classes.title}>
+                    Goles
+                  </Text>
+                  <p></p>
+                  <Title order={4} className={classes.title}>
+                    Granero
+                  </Title>
+                </div>
+                <div>
+                  <Image height={rem(320)} className='imageCard' src="	https://kingsleague.pro/wp-content/uploads/2023/05/1K_7.png" alt="Random image" />
+                </div>
               </div>
-            </div>
-          </Paper>
-          <Paper
-            shadow="md"
-            p="xl"
-            radius="md"
-            sx={{ backgroundImage: `url(https://kingsleague.pro/wp-content/uploads/2022/12/1k-bg.jpg)` }}
-            className={classes.card}
-          >
-            <div style={{display: "flex", flexDirection: "row"}}>
-              <div>
-                <Text className={classes.category} size="xs">
-                  Pepe
-                </Text>
-                <Title order={3} className={classes.title}>
-                  Pepito
-                </Title>
+            </Paper>
+          </div>
+          <div style={{ flex: '1' }}>
+            <Paper
+              shadow="md"
+              p="xl"
+              radius="md"
+              sx={{ backgroundImage: `url(https://kingsleague.pro/wp-content/uploads/2022/12/1k-bg.jpg)` }}
+              className={'cardMain'}
+            >
+              <div style={{display: "flex", flexDirection: "row"}}>
+                <div>
+                  <Text className={classes.category} size="xs">
+                    3
+                  </Text>
+                  <Text order={5} className={classes.title}>
+                    Asistencias
+                  </Text>
+                  <p></p>
+                  <Title order={4} className={classes.title}>
+                    Ricardo
+                  </Title>
+                </div>
+                <div>
+                  <Image height={rem(320)} className='imageCard' src="https://kingsleague.pro/wp-content/uploads/2023/05/1K_01.png" alt="Random image" />
+                </div>
               </div>
-              <div>
-                <Image width={"100%"} src="https://kingsleague.pro/wp-content/uploads/2023/05/1K_01.png" alt="Random image" />
-              </div>
-            </div>
-          </Paper>
-          <Paper
-            shadow="md"
-            p="xl"
-            radius="md"
-            sx={{ backgroundImage: `url(https://kingsleague.pro/wp-content/uploads/2022/12/1k-bg.jpg)` }}
-            className={classes.card}
-          >
-            <div style={{display: "flex", flexDirection: "row"}}>
-              <div>
-                <Text className={classes.category} size="xs">
-                  Pepe
-                </Text>
-                <Title order={3} className={classes.title}>
-                  Pepito
-                </Title>
-              </div>
-              <div>
-                <Image width={"100%"} src="https://kingsleague.pro/wp-content/uploads/2023/05/1K_01.png" alt="Random image" />
-              </div>
-            </div>
-          </Paper>
+            </Paper>
+          </div>
         </div>
       </div>
     </div>
