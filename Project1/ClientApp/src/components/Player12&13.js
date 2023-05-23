163ad6ec13e58e33e3294eb494a50c10361ee5f9
abrahamr110 <abrahamrm100@gmail.com>
2023-05-23 20:01:14 +0200

    Update Player12&13.js

--- Project1/ClientApp/src/components/Player12&13.js
+++ Project1/ClientApp/src/components/Player12&13.js
@@ -4,7 +4,7 @@ import { Text, Paper } from '@mantine/core';
 
 const data = [
   {
-    team: 'PIaaaaaaaaaaO FC',
+    team: 'Ultimate Móstoles',
     shield: 'https://www.kingsleague.pro/wp-content/uploads/2022/11/ultimate-mostoles.svg',
     player: 'https://kingsleague.pro/wp-content/uploads/2023/05/ULT_5.png',
     namePlayer: 'Cristian Gómez',
@@ -28,59 +28,148 @@ const data = [
     number:'12'
   },
   {
-    team: 'aaaaaaaaaa',
-    shield: 'https://www.kingsleague.pro/wp-content/uploads/2022/11/xbuyer-team.svg',
-    player: 'https://kingsleague.pro/wp-content/uploads/2023/05/XBU_6.png',
-    namePlayer: 'bbbbbb',
+    team: 'Rayo de Barcelona',
+    shield: 'https://www.kingsleague.pro/wp-content/uploads/2022/11/rayo-barcelona.svg',
+    player: 'https://kingsleague.pro/wp-content/uploads/2023/05/RDB_20.png',
+    namePlayer: 'Dani Liñares',
+    position: 'MEDIO',
+    number:'13'
+  },
+  {
+    team: '1K FC',
+    shield: 'https://www.kingsleague.pro/wp-content/uploads/2022/12/1k-inverse.svg',
+    player: 'https://kingsleague.pro/wp-content/uploads/2023/05/1K_25.png',
+    namePlayer: 'Marc Torrejón',
+    position: 'DEFENSA',
+    number:'13'
+  },
+  {
+    team: 'Porcinos FC',
+    shield: 'https://www.kingsleague.pro/wp-content/uploads/2022/11/porcinos-fc.svg',
+    player: 'https://kingsleague.pro/wp-content/uploads/2023/05/POR_7.png',
+    namePlayer: 'Hugo Fraile',
     position: 'MEDIO',
     number:'12'
   },
   {
-    team: 'aaaaaaaaaa',
-    shield: 'https://www.kingsleague.pro/wp-content/uploads/2022/11/xbuyer-team.svg',
-    player: 'https://kingsleague.pro/wp-content/uploads/2023/05/XBU_6.png',
-    namePlayer: 'bbbbbb',
+    team: 'Jijantes FC',
+    shield: 'https://www.kingsleague.pro/wp-content/uploads/2022/11/jijantes-fc.svg',
+    player: 'https://kingsleague.pro/wp-content/uploads/2023/05/JFC_11.png',
+    namePlayer: 'Ibai Gómez',
     position: 'MEDIO',
     number:'12'
+  },
+  {
+    team: 'Aniquiladores FC',
+    shield: 'https://www.kingsleague.pro/wp-content/uploads/2022/11/aniquiladores.svg',
+    player: 'https://kingsleague.pro/wp-content/uploads/2023/05/ANI_3.png',
+    namePlayer: 'Nadir Louah',
+    position: 'DEFENSA',
+    number:'12'
+  },
+  {
+    team: 'Los Troncos FC',
+    shield: 'https://www.kingsleague.pro/wp-content/uploads/2022/12/los-troncos.png',
+    player: 'https://kingsleague.pro/wp-content/uploads/2023/05/TFC_10.png',
+    namePlayer: 'Joan Verdú',
+    position: 'MEDIO',
+    number:'12'
+  },
+  {
+    team: 'El Barrio',
+    shield: 'https://www.kingsleague.pro/wp-content/uploads/2022/11/el-bbarrio.svg',
+    player: 'https://kingsleague.pro/wp-content/uploads/2023/05/ELB_21.png',
+    namePlayer: 'Nico Pareja',
+    position: 'DEFENSA',
+    number:'12'
+  },
+  {
+    team: 'Kunisports',
+    shield: 'https://www.kingsleague.pro/wp-content/uploads/2022/11/kunisports.svg',
+    player: 'https://kingsleague.pro/wp-content/uploads/2023/05/KNS_32.png',
+    namePlayer: 'M. del Castillo',
+    position: 'DELANTERO',
+    number:'13'
+  },
+  {
+    team: 'Porcinos FC',
+    shield: 'https://www.kingsleague.pro/wp-content/uploads/2022/11/porcinos-fc.svg',
+    player: 'https://kingsleague.pro/wp-content/uploads/2023/05/POR_23.png',
+    namePlayer: 'Alberto Bueno',
+    position: 'DELANTERO',
+    number:'13'
   }
 ];
 
 function Buttonn() {
+  const [mostrarLista, setMostrarLista] = useState(false);
+  const[player12and13,setplayer12and13]=useState(false);
+
+  const handleClick = () => {
+    setMostrarLista(!mostrarLista);
+  };
+  const handleClickPlayer=()=>{
+    setplayer12and13(!player12and13)
+  }
+
   return (
     <div style={{ display: 'flex', flexDirection: 'row', gap: '20px' }}>
       <Button variant="light" color="orange" radius="lg" size="lg">
         Todos
       </Button>
-      <Button variant="light" color="orange" radius="lg" size="lg">
-        Equipos
-      </Button>
-      <Button variant="light" color="orange" radius="lg" size="lg">
+      <div>
+      <Button variant="light" color="orange" radius="lg" size="lg" onClick={handleClick}>Equipos</Button>
+      {mostrarLista && (
+        <ul>
+          <li>1K FC</li>
+          <li>Rayo de Barcelona</li>
+          <li>PIO FC</li>
+          <li>Ultimate Móstoles</li>
+          <li>Kunisports</li>
+          <li>Porcinos FC</li>
+          <li>El Barrio</li>
+          <li>xBuyer Team</li>
+          <li>Los Troncos FC</li>
+        </ul>
+      )}
+      </div>
+      <div>
+      <Button variant="light" color="orange" radius="lg" size="lg" onClick={handleClickPlayer}>
         Jugador
       </Button>
+      {player12and13 && (
+        <ul>
+          <li>Jugador 12</li>
+          <li>Jugador 13</li>
+        </ul>
+      )}
+      </div>
     </div>
   );
 }
 
 function Paperr({ data }) {
   return (
-    <div style={{ minWidth:'max-content'}}>
-      <div style={{ display: 'flex', backgroundImage: 'url(https://kingsleague.pro/wp-content/uploads/2022/12/mostoles-bg.jpg)', backgroundSize: '100%', width: '30%', flexDirection: 'row' }}>
-        <div>
-          <img src={data.player} style={{ width: '100%', marginTop: '20%' }}></img>
+    <div style={{ display:'flex'}}>
+      <div style={{ display: 'flex', background:'linear-gradient(236.07deg,#f7ab35 .09%,#e8521e 102.47%)', backgroundSize: '100%', width: '100%', flexDirection: 'row' }}>
+        <div style={{width:'100%'}}>
+          <img src={data.player} style={{ width: '100%', height: 'auto', maxWidth: '150px', maxHeight: '200px', objectFit: 'cover' }}></img>
         </div>
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', width:'200%' }}>
           <div style={{ display: 'flex', flexDirection: 'row', width: '20%', gap: '30%', alignItems: 'center', marginTop: '3%', height: '30%' }}>
-            <img src={data.shield} style={{ width: '60%' }}></img>
+            <img src={data.shield} style={{ width: '50%', height: 'auto', maxWidth: '150px', maxHeight: '200px', objectFit: 'cover' }}></img>
             <div style={{ minWidth: 'max-content' }}>
               <h5 style={{ color: 'white' }}>{data.team}</h5>
             </div>
-            <button style={{ backgroundColor: 'black', color: 'orange', fontFamily: 'monospace' }}>{data.number}</button>
+            <div>
+              <span style={{ backgroundColor: 'black', color: 'orange', fontFamily: 'monospace', fontSize:'20px'}}>{data.number}</span>
+            </div>
           </div>
           <div style={{ display: 'flex', height: '30%', width: '100%', color: 'white', marginTop: '10px' }}>
             <h3 style={{ fontSize: '30px', fontWeight: 'bold' }}>{data.namePlayer}</h3>
           </div>
           <div style={{ marginTop: '10px', height: '30%', width: '40%' }}>
-            <p style={{ backgroundColor: 'black', color: 'white', textAlign: 'center', fontFamily: "Archivo Narrow" }}>{data.position}</p>
+            <p style={{ backgroundColor: 'black', color: 'white', textAlign: 'center',fontWeight:'bold'}}>{data.position}</p>
           </div>
         </div>
       </div>
@@ -89,11 +178,11 @@ function Paperr({ data }) {
 }
 
 export default function Player1213() {
-  const uniqueData = [...new Map(data.map((item) => [item.team, item])).values()];
+  const uniqueData = [...new Map(data.map((item) => [item.namePlayer, item])).values()];
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', margin: '5%', gap: '20px' }}>
-      <h1>Jugadores 12 y 13</h1>
+      <h1 style={{fontFamily: "Archivo Narrow"}}>Jugadores 12 y 13</h1>
       <div>
         <Buttonn />
       </div>
