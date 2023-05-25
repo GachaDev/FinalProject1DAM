68c2ff848f66435c32f4cdeed73e434632cffd6d
JesusChaves <82162116+GachaDev@users.noreply.github.com>
2023-05-25 11:08:57 +0200

    Update Home.js

--- Project1/ClientApp/src/components/Home/Home.js
+++ Project1/ClientApp/src/components/Home/Home.js
@@ -3,6 +3,9 @@ import CarouselE from './CarouselE/CarouselE'
 import { useMediaQuery } from '@mantine/hooks';
 import { useMantineTheme } from '@mantine/core';
 import BestPlayers from './BestPlayers/BestPlayers';
+import { Title } from '@mantine/core';
+import { Avatar, Badge, Table, Group, Text, Select, ScrollArea } from '@mantine/core';
+
 
 export default function Home() {
   const theme = useMantineTheme();
@@ -11,6 +14,9 @@ export default function Home() {
     <div style={{ display: 'flex', flexDirection: mobile ? 'column' : 'row', margin: 40, gap: 20 }}>
       <div className='homeIzquierda'>
         <Matches />
+        <div className='containerCalendar' style={{marginTop: 20}}>
+          <Title order={4} className='titleCalendar'>Clasificación</Title>
+        </div>
       </div>
       <div className='homeDerecha'>
         <CarouselE />
