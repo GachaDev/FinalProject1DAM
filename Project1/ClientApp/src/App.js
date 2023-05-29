166d846128c1b3de83a675996754e08f372453b0
JesusChaves <82162116+GachaDev@users.noreply.github.com>
2023-05-29 17:18:31 +0200

    logout button

--- Project1/ClientApp/src/App.js
+++ Project1/ClientApp/src/App.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
 import LoggedInComponent from './components/LoggedInComponent';
 import Login from './components/Login/Login';
 import { CSSTransition } from 'react-transition-group';
+import { UseAdmin } from './Zustand/UseAdmin';
 
 export default function App() {
-  const [isLogged, setIsLogged] = useState(false);
+  const { isLogged, setIsLogged } = UseAdmin();
   return (
     <div>
       <CSSTransition
--- Project1/ClientApp/src/Zustand/UseAdmin.js
+++ Project1/ClientApp/src/Zustand/UseAdmin.js
@@ -1,6 +1,8 @@
 import { create } from 'zustand'
 
 export const UseAdmin = create((set) => ({
+    isLogged : false,
     Admin: false,
     setAdmin: (Admin) => set({ Admin }),
+    setIsLogged: (isLogged) => set({ isLogged }),
 }));
