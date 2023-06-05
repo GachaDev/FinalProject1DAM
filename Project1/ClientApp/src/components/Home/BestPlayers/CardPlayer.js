c651274aa64380b2b7c6f8543d7a04282c56f2cd
JesusChaves <82162116+GachaDev@users.noreply.github.com>
2023-06-05 19:48:24 +0200

    Update CardPlayer.js

--- Project1/ClientApp/src/components/Home/BestPlayers/CardPlayer.js
+++ Project1/ClientApp/src/components/Home/BestPlayers/CardPlayer.js
@@ -1,32 +1,52 @@
 import { Paper, Text, Title, Image, rem } from '@mantine/core';
+import { Trash, Edit } from 'tabler-icons-react';
+import { ActionIcon } from '@mantine/core';
+import { UseAdmin } from '../../../Zustand/UseAdmin';
 
 export default function CardPlayer({backUrl, title, category, name, playerImg}) {
-  return (
-    <div style={{ flex: '1' }}>
-        <Paper
-            shadow="md"
-            p="xl"
-            radius="md"
-            sx={{ backgroundImage: `url(${backUrl})` }}
-            className={'cardMain'}
-        >
-            <div style={{display: "flex", flexDirection: "row"}}>
-                <div>
-                    <Text className={'categoryCard'} size="xs">
-                    {title}
-                    </Text>
-                    <Text order={5} className={'titleCard'}>
-                    {category}
-                    </Text>
-                    <Title order={4} className={'titleCard'}>
-                    {name}
-                    </Title>
+    const { Admin } = UseAdmin();
+    return (
+        <div style={{ flex: '1' }}>
+            <Paper
+                shadow="md"
+                p="xl"
+                radius="md"
+                sx={{ backgroundImage: `url(${backUrl})` }}
+                className={'cardMain'}
+            >
+                <div style={{display: "flex", flexDirection: "row"}}>
+                    <div>
+                        <Text className={'categoryCard'} size="xs">
+                        {title}
+                        </Text>
+                        <Text order={5} className={'titleCard'}>
+                        {category}
+                        </Text>
+                        <Title order={4} className={'titleCard'}>
+                        {name}
+                        </Title>
+                        {Admin ? <div style={{ display: 'flex', flexDirection:'row' }}>
+                            <ActionIcon variant="transparent" size="lg">
+                                <Edit
+                                size={20}
+                                strokeWidth={2}
+                                color={'white'}
+                                />
+                            </ActionIcon>
+                            <ActionIcon variant="transparent" size="lg">
+                                <Trash
+                                size={20}
+                                strokeWidth={2}
+                                color={'white'}
+                                />
+                            </ActionIcon>
+                        </div> : null}
+                    </div>
+                    <div>
+                        <Image height={rem(320)} className='imageCard' src={playerImg} alt="Random image" />
+                    </div>
                 </div>
-                <div>
-                    <Image height={rem(320)} className='imageCard' src={playerImg} alt="Random image" />
-                </div>
-            </div>
-        </Paper>
-    </div>
-  );
+            </Paper>
+        </div>
+    );
 }
